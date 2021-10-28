c58af8704a4b0abceb8770068d218298a75b762f
wdesimini <wdesimini8@gmail.com>
2021-10-27 17:14:51 -0700

    start mvc module inside concrete controller

--- MVC.xctemplate/___FILEBASENAME___/___FILEBASENAME___Controller.swift
+++ MVC.xctemplate/___FILEBASENAME___/___FILEBASENAME___Controller.swift
@@ -10,6 +10,17 @@ import Foundation
 class ___FILEBASENAMEASIDENTIFIER___ {
     var model: ___VARIABLE_productName___ModelInput!
     weak var output: ___VARIABLE_productName___ControllerOutput!
+    
+    func start___VARIABLE_productName___() -> ___VARIABLE_productName___ViewInput {
+        let model = ___VARIABLE_productName___Model()
+        let controller = self
+        let view = ___VARIABLE_productName___View()
+        model.output = controller
+        controller.model = model
+        controller.output = view
+        view.controller = controller
+        return view
+    }
 }
 
 extension ___FILEBASENAMEASIDENTIFIER___: ___VARIABLE_productName___ControllerInput {
