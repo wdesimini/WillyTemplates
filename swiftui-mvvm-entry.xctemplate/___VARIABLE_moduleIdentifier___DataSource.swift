7e641057b93575d291c83f32d92ec4aeedded1c3
Wilson Desimini <wdesimini8@gmail.com>
2022-04-28 08:29:09 -0700

    add missing datasource file to swiftui-mvvm-entry

---
+++ swiftui-mvvm-entry.xctemplate/___VARIABLE_moduleIdentifier___DataSource.swift
@@ -0,0 +1,25 @@
+//
+//  ___FILENAME___
+//  ___PROJECTNAME___
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___.
+//
+
+protocol ___VARIABLE_moduleIdentifier___EntryDataSource {
+    func empty___VARIABLE_moduleIdentifier___() -> ___VARIABLE_moduleIdentifier___
+    func submit___VARIABLE_moduleIdentifier___(
+        _ ___VARIABLE_objectIdentifier___: ___VARIABLE_moduleIdentifier___
+    )
+}
+
+extension DataManager: ___VARIABLE_moduleIdentifier___EntryDataSource {
+    func empty___VARIABLE_moduleIdentifier___() -> ___VARIABLE_moduleIdentifier___ {
+        ___VARIABLE_moduleIdentifier___(id: .init())
+    }
+    
+    func submit___VARIABLE_moduleIdentifier___(
+        _ ___VARIABLE_objectIdentifier___: ___VARIABLE_moduleIdentifier___
+    ) {
+        ___VARIABLE_objectIdentifier___DataService.update(___VARIABLE_objectIdentifier___)
+    }
+}
