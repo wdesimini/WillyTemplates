88490c5ea7837aa31baf28a26249d5df06564980
Wilson Desimini <wdesimini8@gmail.com>
2022-04-28 08:27:17 -0700

    update swiftui-mvvm-entry for abstracted datasource, delegate

--- swiftui-mvvm-entry.xctemplate/___VARIABLE_moduleIdentifier___EntryView.swift
+++ swiftui-mvvm-entry.xctemplate/___VARIABLE_moduleIdentifier___EntryView.swift
@@ -3,20 +3,37 @@
 import SwiftUI
 
 struct ___VARIABLE_moduleIdentifier___EntryView: View {
-    @ObservedObject var viewModel: ViewModel
+    @ObservedObject var viewModel: ___VARIABLE_moduleIdentifier___EntryViewModel
     
     var body: some View {
         VStack {
-            Button("Save", action: viewModel.save___VARIABLE_moduleIdentifier___)
+            Button(
+                "Submit",
+                action: viewModel.submit___VARIABLE_moduleIdentifier___
+            )
         }
         .padding()
     }
 }
 
 struct ___VARIABLE_moduleIdentifier___EntryView_Previews: PreviewProvider {
+    static var viewModelDelegate = ___VARIABLE_moduleIdentifier___EntryViewModelPreviewDelegate()
+    
     static var previews: some View {
-        let data = PreviewDataManager.preview
-        let viewModel = ___VARIABLE_moduleIdentifier___EntryView.ViewModel(data: data) { _ in }
-        return ___VARIABLE_moduleIdentifier___EntryView(viewModel: viewModel)
+        ___VARIABLE_moduleIdentifier___EntryView(
+            viewModel: viewModelDelegate.viewModel
+        )
+    }
+}
+
+private class ___VARIABLE_moduleIdentifier___EntryViewModelPreviewDelegate: ___VARIABLE_moduleIdentifier___EntryViewModelDelegate {
+    lazy var viewModel: ___VARIABLE_moduleIdentifier___EntryViewModel =
+    ___VARIABLE_moduleIdentifier___EntryViewModel(
+        dataSource: PreviewDataManager.preview,
+        delegate: self
+    )
+    
+    func ___VARIABLE_objectIdentifier___EntryDidSubmit___VARIABLE_moduleIdentifier___() {
+        
     }
 }
--- swiftui-mvvm-entry.xctemplate/___VARIABLE_moduleIdentifier___EntryViewModel.swift
+++ swiftui-mvvm-entry.xctemplate/___VARIABLE_moduleIdentifier___EntryViewModel.swift
@@ -8,23 +8,33 @@
 import Combine
 import SwiftUI
 
-extension ___VARIABLE_moduleIdentifier___EntryView {
-    class ViewModel: ObservableObject {
-        private let handler: (___VARIABLE_moduleIdentifier___.ID?) -> Void
-        private let service: DataService<___VARIABLE_moduleIdentifier___>
-        
-        init(data: DataManager, handler: @escaping (___VARIABLE_moduleIdentifier___.ID?) -> Void) {
-            self.service = data.___VARIABLE_objectIdentifier___Service
-            self.handler = handler
-        }
-        
-        func save___VARIABLE_moduleIdentifier___() {
-            do {
-                let ___VARIABLE_objectIdentifier___ = try service.createNew___VARIABLE_moduleIdentifier___()
-                handler(___VARIABLE_objectIdentifier___.id)
-            } catch {
-                print(error.localizedDescription)
-            }
-        }
+protocol ___VARIABLE_moduleIdentifier___EntryViewModelDelegate {
+    func ___VARIABLE_objectIdentifier___EntryDidSubmit___VARIABLE_moduleIdentifier___()
+}
+
+class ___VARIABLE_moduleIdentifier___EntryViewModel {
+    private let dataSource: ___VARIABLE_moduleIdentifier___DataSource
+    unowned private let delegate: ___VARIABLE_moduleIdentifier___EntryViewModelDelegate
+    @Published var ___VARIABLE_objectIdentifier___: ___VARIABLE_moduleIdentifier___
+    
+    init(
+        dataSource: ___VARIABLE_moduleIdentifier___DataSource,
+        delegate: ___VARIABLE_moduleIdentifier___EntryViewModelDelegate,
+        ___VARIABLE_objectIdentifier: ___VARIABLE_moduleIdentifier___? = nil
+    ) {
+        self.dataSource = dataSource
+        self.delegate = delegate
+        self.___VARIABLE_objectIdentifier___ =
+        ___VARIABLE_objectIdentifier___
+        ?? dataSource.empty___VARIABLE_moduleIdentifier___()
+    }
+    
+    func cancel___VARIABLE_moduleIdentifier___Entry() {
+        delegate.___VARIABLE_objectIdentifier___EntryDidSubmit___VARIABLE_moduleIdentifier___()
+    }
+    
+    func submit___VARIABLE_moduleIdentifier___() {
+        dataSource.submit___VARIABLE_moduleIdentifier___(___VARIABLE_objectIdentifier___)
+        delegate.___VARIABLE_objectIdentifier___EntryDidSubmit___VARIABLE_moduleIdentifier___()
     }
 }
