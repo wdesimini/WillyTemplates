e1404d336fa517cea5ce64573330cc080361c9e1
Wilson Desimini <wdesimini8@gmail.com>
2022-06-25 10:59:47 -0700

    reorganize templates

---
+++ willy-swiftui/swiftui-mvvm-entry.xctemplate/___VARIABLE_moduleIdentifier___+Empty.swift
@@ -0,0 +1,12 @@
+//
+//  ___FILENAME___
+//  ___PROJECTNAME___
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___.
+//
+
+extension ___VARIABLE_moduleIdentifier___ {
+    static func empty() -> ___VARIABLE_moduleIdentifier___ {
+        .init(id: .init())
+    }
+}
