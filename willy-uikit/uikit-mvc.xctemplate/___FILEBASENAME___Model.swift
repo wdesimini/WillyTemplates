bb538aa0d55d96eb78f40cca344a18c5341343dd
Wilson Desimini <wdesimini8@gmail.com>
2022-07-12 14:26:21 -0700

    move uikit templates to separate folder

---
+++ willy-uikit/uikit-mvc.xctemplate/___FILEBASENAME___Model.swift
@@ -0,0 +1,13 @@
+//
+//  ___FILENAME___
+//  ___PROJECTNAME___
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___.
+//  ___COPYRIGHT___
+
+class ___FILEBASENAMEASIDENTIFIER___ {
+    weak var output: ___VARIABLE_productName___ModelOutput!
+}
+
+extension ___VARIABLE_productName___Model: ___VARIABLE_productName___ModelInput {
+}
---
+++ willy-uikit/uikit-mvc.xctemplate/___FILEBASENAME___View.swift
@@ -0,0 +1,26 @@
+//
+//  ___FILENAME___
+//  ___PROJECTNAME___
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___.
+//  ___COPYRIGHT___
+
+import UIKit
+
+class ___FILEBASENAMEASIDENTIFIER___: UIViewController {
+    var controller: ___VARIABLE_productName___ControllerInput!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        add___VARIABLE_productName___Views()
+    }
+    
+    private func add___VARIABLE_productName___Views() {
+    }
+}
+
+extension ___FILEBASENAMEASIDENTIFIER___: ___VARIABLE_productName___ViewInput {
+}
+
+extension ___FILEBASENAMEASIDENTIFIER___: ___VARIABLE_productName___ControllerOutput {
+}
