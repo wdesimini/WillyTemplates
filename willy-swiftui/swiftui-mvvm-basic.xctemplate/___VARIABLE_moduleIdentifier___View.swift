5b9d7b96831fde3e337fd61e4a95b7b175eef3bf
Wilson Desimini <wdesimini8@gmail.com>
2022-07-24 10:21:59 -0700

    clean-up swiftui-mvvm-basic template

--- willy-swiftui/swiftui-mvvm-basic.xctemplate/___VARIABLE_moduleIdentifier___View.swift
+++ willy-swiftui/swiftui-mvvm-basic.xctemplate/___VARIABLE_moduleIdentifier___View.swift
@@ -3,16 +3,32 @@
 import SwiftUI
 
 struct ___FILEBASENAMEASIDENTIFIER___: View {
-    let viewModel: ___VARIABLE_moduleIdentifier___ViewModel
+
+    // MARK: State
+
+    private let viewModel: ___VARIABLE_moduleIdentifier___ViewModel
+
+    // MARK: Initialization
+
+    init(viewModel: ___VARIABLE_moduleIdentifier___ViewModel) {
+        self.viewModel = viewModel
+    }
+
+    // MARK: Body
 
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Text("")
     }
 }
 
-struct ___VARIABLE_moduleIdentifier___View_Previews: PreviewProvider {
+// MARK: Preview
+
+struct ___FILEBASENAMEASIDENTIFIER____Previews: PreviewProvider {
     static var previews: some View {
-        let viewModel = ___VARIABLE_moduleIdentifier___ViewModel()
-        return ___VARIABLE_moduleIdentifier___View(viewModel: viewModel)
+        ___FILEBASENAMEASIDENTIFIER___(viewModel: viewModel)
+    }
+
+    private static var viewModel: ___VARIABLE_moduleIdentifier___ViewModel {
+        .init()
     }
 }
