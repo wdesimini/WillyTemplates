37e886214990bf9205ca12a4ff31d30014bb3891
Wilson Desimini <wdesimini8@gmail.com>
2023-02-22 15:57:10 -0800

    add tca templates

---
+++ willy-tca/tca.xctemplate/___VARIABLE_moduleIdentifier___.swift
@@ -0,0 +1,15 @@
+// ___FILEHEADER___
+
+import ComposableArchitecture
+
+struct ___FILEBASENAMEASIDENTIFIER___: ReducerProtocol {
+    struct State: Equatable {
+    }
+
+    enum Action: Equatable {
+    }
+
+    func reduce(into state: inout State, action: Action) -> EffectTask<Action> {
+        return .none
+    }
+}
---
+++ willy-tca/tca.xctemplate/___VARIABLE_moduleIdentifier___View.swift
@@ -0,0 +1,25 @@
+// ___FILEHEADER___
+
+import ComposableArchitecture
+import SwiftUI
+
+struct ___FILEBASENAMEASIDENTIFIER___: View {
+    let store: StoreOf<___VARIABLE_moduleIdentifier___>
+
+    var body: some View {
+        WithViewStore(store) { store in 
+            EmptyView()
+        }
+    }
+}
+
+struct ___FILEBASENAMEASIDENTIFIER____Previews: PreviewProvider {
+    static var previews: some View {
+        ___FILEBASENAMEASIDENTIFIER___(
+            store: .init(
+                initialState: ___VARIABLE_moduleIdentifier___.State(), 
+                reducer: ___VARIABLE_moduleIdentifier___()
+            )
+        )
+    }
+}
